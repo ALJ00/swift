b59cde6cbffc9972930d165764e7ed7c95eec9c9
josearmas <armasjose1980@gmail.com>
2018-09-26 18:56:57 +0200

    Ejercicio2_dias_semana

---
+++ 03_ejercicios/02_dia_semana/diaSemana.playground/Contents.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+var str = "Hello, playground"
+
+let dia = 1
+
+
+switch dia {
+case 1:
+    print ("lunes")
+case 2:
+    print("Martes")
+case 3:
+    print("Miercoles")
+case 4:
+    print("jueves")
+case 5:
+    print("viernes")
+case 6:
+    print("sábado")
+case 7:
+    print("Domingo")
+default:
+    print("Domingo")
+}
+
+
+
+
+
+if dia==1 {
+    print ("lunes")
+}else if dia == 2{
+    print ("martes")
+}else if dia == 3{
+    print ("miercoles")
+}else if dia == 4{
+    print("jueves")
+}else if dia == 5 {
+    print("viernes")
+}else if dia == 6 {
+    print("sábado")
+}else{
+    print("domingo")
+}
